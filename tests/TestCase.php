236a1ee04c4b104960a07a0ba818b6d3b8ee8cf6
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-15 01:33:11 +0300

    math test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -12,15 +12,16 @@ abstract class TestCase extends Laravel\Lumen\Testing\TestCase
         return require __DIR__.'/../bootstrap/app.php';
     }
 
-    public function setUp()
-    {
-        parent::setUp();
-        Artisan::call('migrate');
-    }
-
-    public function tearDown()
-    {
-        Artisan::call('migrate:reset');
-        parent::tearDown();
-    }
+    // public function setUp()
+    // {
+    //     parent::setUp();
+    //
+    //     Artisan::call('migrate');
+    // }
+    //
+    // public function tearDown()
+    // {
+    //     Artisan::call('migrate:reset');
+    //     parent::tearDown();
+    // }
 }
